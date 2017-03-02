e382666da2e162a23d3e604b985560527c9f1f4e
Ken Brown <kbrown@cornell.edu>
2017-03-02 16:45:38 -0500

    Set up for Appveyor build

---
+++ appveyor.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+
+echo fetching...
+cygport clisp.cygport fetch
+
+echo prepping...
+cygport clisp.cygport prep
+
+echo compiling...
+cygport clisp.cygport compile
+
+echo installing...
+cygport clisp.cygport inst
+
+echo packaging...
+cygport clisp.cygport pkg
+
+# echo testing...
+# cygport clisp.cygport test
+
+tar -cJf artifact.tar.xz clisp-*/dist clisp-*/log
